ed9627c5dc37aec83f8d3d133748c4bec252e2b0
diredocks <26994007+diredocks@users.noreply.github.com>
2024-09-27 19:11:16 +0800

    vm: starnger...

--- 07_basic-vm/vm.py
+++ 07_basic-vm/vm.py
@@ -12,35 +12,41 @@ def read_source(file_name):
         source = [source_beauty(line) for line in lines]
     return source
 
-def arithmetic_op(operator):
-    return \
+snippets = {
+    "jump_to_last_operand": ["@SP", "A=M-1"],
+    "jump_up_operand": ["D=M", "A=A-1"],
+    "save_stack_top_position_uni": ["D=A+1", "@SP", "M=D"],
+    "save_stack_top_position_pop": ["@SP", "M=M-1"],
+    "save_stack_top_position_push": ["@SP", "M=M+1"],
+}
+
+snippets_lam = {
+    "constant_push":\
+        lambda i:\
+        [f"@{i}", "D=A", "@SP", "A=M", "M=D"]\
+        + snippets["save_stack_top_position_push"],
+    "comparison_op":\
+        lambda operator, label_i:\
+        snippets["jump_to_last_operand"]\
+        + snippets["jump_up_operand"]\
+        + ["M=M-D"]\
+        + snippets["save_stack_top_position_pop"]\
+        + ["A=M-1", "D=M", f"@cmp{label_i}", f"D;{operator}"]\
+        + snippets["jump_to_last_operand"] + ["M=0"]\
+        + [f"@ed{label_i}", "0;JMP"]\
+        + [f"(cmp{label_i})"] + snippets["jump_to_last_operand"]\
+        + ["M=-1", f"(ed{label_i})"],
+    "arithmetic_op":\
+        lambda operator:\
         snippets["jump_to_last_operand"]\
         + snippets["jump_up_operand"]\
         + ([f"M=M{operator}D"] if operator == "-"\
         else [f"M=D{operator}M"])\
-        + snippets["save_stack_top_position"]
-
-def comparison_op(operator, label_i):
-    return \
+        + snippets["save_stack_top_position_pop"],
+    "unary_op":\
+        lambda operator:\
         snippets["jump_to_last_operand"]\
-        + snippets["jump_up_operand"]\
-        + ["M=M-D"]\
-        + snippets["save_stack_top_position"]\
-        + ["A=D-1", "D=M", f"@cmp{label_i}", f"D;{operator}", "@SP", "A=M-1", "M=0"]\
-        + [f"@ed{label_i}", "0;JMP"]\
-        + [f"(cmp{label_i})", "@SP", "A=M-1", "M=-1"]\
-        + [f"(ed{label_i})"]
-
-def unary_op(operator):
-    return \
-        snippets["jump_to_last_operand"]\
-        + [f"M={operator}M"]\
-
-snippets = {
-    "jump_to_last_operand": ["@SP", "A=M-1"],
-    "jump_up_operand": ["D=M", "A=A-1"],
-    "save_stack_top_position": ["D=A+1", "@SP", "M=D"],
-    "constant_push": lambda i: [f"@{i}", "D=A", "@SP", "A=M", "M=D"]
+        + [f"M={operator}M"]
 }
 
 operator_map = {
@@ -64,33 +70,11 @@ operator_map = {
     }
 }
 
-def writer(line, label_i):
-    cmd = line[0]
-    if cmd in operator_map["arithmetic_op"].keys():
-        return arithmetic_op(
-                operator_map["arithmetic_op"][cmd]
-                )
-    elif cmd in operator_map["comparison_op"].keys():
-        return comparison_op(
-                operator_map["comparison_op"][cmd], label_i
-                )
-    elif cmd in operator_map["unary_op"].keys():
-        return unary_op(
-                operator_map["unary_op"][cmd]
-                )
-    elif cmd == "push":
-        if line[1] == "constant":
-            return snippets["constant_push"](line[2])\
-                    + snippets["save_stack_top_position"]
-    else:
-        return None
-
 def main():
     label_i = 0
     source = read_source("./simple_add.vm")
-    for each in source:
-        print("\n".join(writer(each, label_i)))
-        label_i = label_i + 1
+    for key in operator_map:
+        print("eq" in operator_map[key].keys())
 
 if __name__ == "__main__":
     main()
